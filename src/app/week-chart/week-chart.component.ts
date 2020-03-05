c7cf9ed0920cb73a0383d7a63b455c29fba9182b
Cristian <cristian97drb@gmail.com>
2020-03-05 11:23:24 -0400

    add another chart

--- src/app/week-chart/week-chart.component.ts
+++ src/app/week-chart/week-chart.component.ts
@@ -12,7 +12,7 @@ export class WeekChartComponent implements OnInit {
   single: any[];
   multi: any[];
 
-  view: any[] = [700, 400];
+  view: any[] = [350, 400];
 
   // options
   showXAxis = true;
